efecfbd6e13a42c6542ee5adad1c920962fef13b
merlou52 <61518160+merlou52@users.noreply.github.com>
2021-04-21 18:08:23 +0200

    Initialisation Vue.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import router from './router'
 
 Vue.config.productionTip = false
 
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+Vue.use(VueRouter)
+
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
